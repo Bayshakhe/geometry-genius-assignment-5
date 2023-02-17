3dc2be5d97d64d44c6d635b5dc6777125662345e
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-02-18 03:59:19 +0600

    complete the blog.html file

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById('blog-btn').addEventListener('click', function(){
+    document.location.href = 'blog.html'
+})
+
+// document.getElementById('geometry-btn').addEventListener('click', function(){
+//     window.location.href = 'index.html'
+//     console.log(true)
+// })
+
+
+document.getElementById('geometry-btn').addEventListener('click', function(){
+    window.location.href = 'http://127.0.0.1:5500/index.html';
+})
