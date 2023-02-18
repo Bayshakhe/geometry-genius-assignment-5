b084bab898482c855d2b7b2ef736da1ec2b16a47
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-02-18 23:51:59 +0600

    clear the input field for not enter a number

--- ractangle.js
+++ ractangle.js
@@ -5,8 +5,8 @@ document.getElementById('rectangle-calculate').addEventListener('click', functio
 
     if(isNaN(widthInputValue) === true || isNaN(lengthInputValue) === true || widthInputValue <= 0 || lengthInputValue <= 0) {
         alert('Please input a valid number');
-        widthInputValue = '';
-        lengthInputValue = '';
+        document.getElementById('ractangle-width-input').value = '';
+        document.getElementById('ractangle-length-input').value = '';
     }
     else{
         const ractangleValue = widthInputValue * lengthInputValue;
